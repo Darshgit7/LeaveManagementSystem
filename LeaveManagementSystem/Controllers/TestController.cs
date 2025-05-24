ea86b68005d1cde989758de8f9003fb4e9df8710
darsh <darsh@DARSH-7D>
2025-05-24 12:27:34 +0530

    Add project files.

---
+++ LeaveManagementSystem/Controllers/TestController.cs
@@ -0,0 +1,18 @@
+﻿using LeaveManagementSystem.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace LeaveManagementSystem.Controllers
+{
+    public class TestController : Controller
+    {
+        public IActionResult Index()
+        {
+            var data = new TestViewModel
+            {
+                Name = "Student",
+                DateOfBirth = new DateTime(1992,07,12)
+            };
+            return View(data);
+        }
+    }
+}
